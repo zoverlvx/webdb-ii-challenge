ebb8b66faf41a79775d93f0c740fe37b23d03aaf
Zach Overhulser <zoverhulser@gmail.com>
2019-05-14 21:51:14 +0000

    Tests posting

--- scripts/test_post.sh
+++ scripts/test_post.sh
@@ -1,1 +1,1 @@
-curl -d '{"name": "Name of Zoo"}' -H "Content-Type: applications/json" -X POST "https://lambda-school-2-zoverlvx.c9users.io/api/zoos"
+curl -d '{"name": "'$1'"}' -H "Content-Type: application/json" -X POST "https://lambda-school-2-zoverlvx.c9users.io/api/zoos"
